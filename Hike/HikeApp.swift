75ede8da065092b977ed567e049f8672d065c2ac
Diego Martinez <dmartinez@apexsystems.com>
2023-08-16 10:35:11 -0700

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Diego Martinez on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
